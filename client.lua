8a020e12f40470f218a8c37d32387f64b0c03c66
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-14 14:38:57 +0100

    ajout pnj+ base

--- client.lua
+++ client.lua
@@ -4,6 +4,7 @@ local gerantped
 Citizen.CreateThread(function()
     local pedModel = "a_m_y_beach_03"
     local x, y, z = -187.041748, -1586.993408, 34.907715
+    local heading = 235.0
 
     RequestModel(GetHashKey(pedModel))
     while not HasModelLoaded(GetHashKey(pedModel)) do
@@ -15,9 +16,8 @@ Citizen.CreateThread(function()
     end
 
     if not spawned then
-        gerantped = CreatePed(4, GetHashKey(pedModel), x, y, z - 1.0, 0.0, true, true)
-        if DoesEntityExist(gerantped) then
-        else
+        gerantped = CreatePed(4, GetHashKey(pedModel), x, y, z - 1.0, heading, true, true)
+        if not DoesEntityExist(gerantped) then
             return
         end
 
@@ -35,7 +35,39 @@ Citizen.CreateThread(function()
         SetPedRelationshipGroupHash(gerantped, GetHashKey("CIVMALE"))
         SetBlockingOfNonTemporaryEvents(gerantped, true)
         SetPedCanRagdoll(gerantped, false)
+        SetEntityInvincible(gerantped, true)
         SetModelAsNoLongerNeeded(GetHashKey(pedModel))
-        TaskStartScenarioInPlace(gerantped, "WORLD_HUMAN_STAND_IMPATIENT", 0, true)
+        TaskStartScenarioInPlace(gerantped, "WORLD_HUMAN_SMOKING", 0, true)
     end
 end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if spawned and DoesEntityExist(gerantped) then
+            local coords = GetEntityCoords(gerantped)
+            DrawText3D(coords.x - 0.45, coords.y, coords.z + 1.0, "Gérant du Four")  -- Affiche le texte au-dessus du PNJ
+        end
+    end
+end)
+
+function DrawText3D(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    local p = GetGameplayCamCoords()
+    local distance = Vdist(p.x, p.y, p.z, x, y, z)
+    local scale = (1 / distance) * 2.5
+    local fov = (1 / GetGameplayCamFov()) * 100
+    scale = scale * fov
+
+    if onScreen then
+        SetTextScale(0.0 * scale, 0.55 * scale)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        AddTextComponentString(text)
+        DrawText(_x, _y)
+        local factor = (string.len(text)) / 370
+        -- DrawRect(_x, _y + 0.0125 * scale, 0.005 + factor, 0.03 * scale, 0, 0, 0, 100)
+    end
+end
